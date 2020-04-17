311a815983728cff733c51f0145b907dfdcb9acf
austin-coding <59863806+austin-coding@users.noreply.github.com>
2020-04-16 21:02:12 -0500

    Update classes.js

--- js-basics/classes.js
+++ js-basics/classes.js
@@ -18,3 +18,9 @@ class Car  {
 let honda = new Car('Honda civic', 2020, "red")
 console.log({honda})
 console.log(honda.isMyCarCool())
+
+// HW
+// Create a class Called Animal
+// Animal should have the following constructor (name, species, age)
+// Create a method called isAnimalYoung() if the animal is older than 10, then say "yes this animal is young" otherwise say "no this animal is old af"
+// Create another method that will return the animal as a JSON string
